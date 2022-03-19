aa9b022c438998d3f23fca2c7e288664c4161b69
andres-condezo <ra.condezomonge@gmail.com>
2022-03-18 23:57:53 -0500

    Add img, name and price to item

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -2,9 +2,14 @@ import { Component } from 'react';
 
 class Product extends Component {
   render() {
-    console.log(this.props);
+    const { product } = this.props;
+
     return (
-      <p>Item</p>
+      <div>
+        <img src={product.img} alt={product.name}/>
+        <h3>{product.name}</h3>
+        <p>{product.price}</p>
+      </div>
     )
   }
 }
